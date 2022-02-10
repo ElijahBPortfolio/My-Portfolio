d3f7219299f4eed50565df807689612a08d3afc0
Elijah <69411262+ElijahBPortfolio@users.noreply.github.com>
2022-02-10 09:59:01 -0500

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
 $(".popcodeQuestImage").hover(function() {
-    $(".popcodeQuestDescription").show();
     $(".popcodeQuestImage").css("opacity","0.5");
 	});
 
 $(".popcodeQuestImage").mouseleave(function() {
-    $(".popcodeQuestDescription").hide();
     $(".popcodeQuestImage").css("opacity","1");
 });
 
 $(".vehicleQuizImage").hover(function() {
-    $(".vehicleQuizDescription").show();
     $(".vehicleQuizImage").css("opacity","0.5");
 });
 
 $(".vehicleQuizImage").mouseleave(function() {
-    $(".vehicleQuizDescription").hide();
     $(".vehicleQuizImage").css("opacity","1");
 });
 
 $(".birdPicturesImage").hover(function() {
-    $(".birdPicturesDescription").show();
     $(".birdPicturesImage").css("opacity","0.5");
 });
 
 $(".birdPicturesImage").mouseleave(function() {
-    $(".birdPicturesDescription").hide();
     $(".birdPicturesImage").css("opacity","1");
 });
